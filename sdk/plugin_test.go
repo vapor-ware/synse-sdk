b929b86cc40fd8cb531102499490b0b0326fec5d
Erick Daniszewski <edaniszewski@gmail.com>
2017-12-12 15:22:42 -0500

    small clean up

--- sdk/plugin_test.go
+++ sdk/plugin_test.go
@@ -1,15 +1,1 @@
 package sdk
-//
-//import (
-//	"testing"
-//)
-//
-//func TestHandler(t *testing.T) {
-//	h := Handlers{}
-//	if h.Plugin == nil {
-//		t.Error("plugin is nil")
-//	}
-//	if h.Device == nil {
-//		t.Error("device is nil")
-//	}
-//}
