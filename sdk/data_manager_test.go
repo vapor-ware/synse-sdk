483be4715ce4437fdee7e4c6cfa8cb684ed01185
Erick Daniszewski <edaniszewski@gmail.com>
2018-05-02 12:12:01 -0400

    adds some tests

--- sdk/data_manager_test.go
+++ sdk/data_manager_test.go
@@ -77,3 +77,67 @@ func TestNewDataManager2(t *testing.T) {
 	assert.Equal(t, 500, cap(d.readChannel))
 	assert.Equal(t, h, d.handlers)
 }
+
+// TestNewDataManager_NilPlugin tests creating a new dataManager instance, passing
+// in a nil Plugin to the constructor.
+func TestNewDataManager_NilPlugin(t *testing.T) {
+	d, err := newDataManager(nil)
+	assert.Nil(t, d)
+	assert.Error(t, err)
+}
+
+// TestNewDataManager_NilPluginHandlers tests creating a new dataManager instance,
+// passing in a Plugin with nil handlers to the constructor.
+func TestNewDataManager_NilPluginHandlers(t *testing.T) {
+	p := Plugin{
+		handlers: nil,
+	}
+
+	d, err := newDataManager(&p)
+	assert.Nil(t, d)
+	assert.Error(t, err)
+}
+
+// TestNewDataManager_NilPluginConfig tests creating a new dataManager instance,
+// passing in a Plugin with nil config to the constructor.
+func TestNewDataManager_NilPluginConfig(t *testing.T) {
+	p := Plugin{
+		handlers: &Handlers{DeviceID, nil},
+	}
+
+	d, err := newDataManager(&p)
+	assert.Nil(t, d)
+	assert.Error(t, err)
+}
+
+// TestDataManager_WritesEnabled tests that writes are enabled in the dataManager
+// when they are enabled in the config.
+func TestDataManager_WritesEnabled(t *testing.T) {
+	// Create the plugin
+	c := config.PluginConfig{
+		Name:    "test",
+		Version: "test",
+		Network: config.NetworkSettings{
+			Type:    "tcp",
+			Address: "test",
+		},
+		Settings: config.Settings{
+			Read:        config.ReadSettings{Buffer: 200},
+			Write:       config.WriteSettings{
+				Enabled: true,
+				Buffer: 200,
+			},
+			Transaction: config.TransactionSettings{TTL: "2s"},
+		},
+	}
+	p := Plugin{
+		Config: &c,
+		handlers: &Handlers{DeviceID, nil},
+	}
+
+	// Create the dataManager
+	d, err := newDataManager(&p)
+	assert.NoError(t, err)
+
+	assert.True(t, d.writesEnabled())
+}
