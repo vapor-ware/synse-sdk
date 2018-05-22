7e3307c25fa9c36e9dc1974a2d92c08a8726a186
Erick Daniszewski <edaniszewski@gmail.com>
2018-05-22 13:48:17 -0400

    .

--- sdk/cfg/validation.go
+++ sdk/cfg/validation.go
@@ -7,107 +7,124 @@ import (
 	"github.com/vapor-ware/synse-sdk/sdk/logger"
 )
 
-/*
-TODO:
----------------
-- implement util for annotating struct fields with version info
-- implement something to walk a struct and:
-	- get the config version.
-		- if no version, use the latest version
-	- validate that the version is supported
-	- go through all fields/sub-fields of the config struct
-		- check if a value is present
-			- if so, get the version tags, we should support: addedIn, deprecatedIn, removedIn
-				- check if the scheme version is out of bounds for any of
-				  those constraints. If so, log/error.
-			- if not, move on
-*/
-
-func ValidateFieldsForVersion(version *SchemeVersion, config interface{}) error {
-	cfg := reflect.ValueOf(config)
-
-	if cfg.Kind() != reflect.Ptr {
-		return fmt.Errorf("field validation: config struct must specified as a pointer")
-	}
-
-	v := cfg.Elem()
-	t := v.Type()
-	return walkStructFields(version, v, t)
-
+// SchemeValidator is used to validate the scheme of a config.
+type SchemeValidator struct {
+	Version *SchemeVersion
 }
 
-func walkStructFields(version *SchemeVersion, v reflect.Value, t reflect.Type) error {
-	for i := 0; i < t.NumField(); i++ {
-		field := v.Field(i)
-		typeField := t.Field(i)
+// NewSchemeValidator creates a new instance of a SchemeValidator for the
+// specified SchemeVersion.
+func NewSchemeValidator(version *SchemeVersion) *SchemeValidator {
+	return &SchemeValidator{
+		Version: version,
+	}
+}
 
-		// --- Break this out into its own function -------
+func (validator *SchemeValidator) ValidateConfig(config interface{}) error {
+	cfg := reflect.ValueOf(config)
+	if cfg.Kind() != reflect.Ptr {
+		return fmt.Errorf("field validation: config stcut must be specified as a pointer")
+	}
+	return validator.walk(cfg.Elem())
+}
 
-		// We should only care about validation if the field is set.
-		if !isEmptyValue(field) {
-			// Check the "addedIn" tag
-			tag := typeField.Tag.Get(tagAddedIn)
-			if tag != "" {
-				addedInScheme, err := NewSchemeVersion(tag)
-				if err != nil {
-					return err
-				}
-				// FIXME - perhaps an IsLessThan() function would be better
-				if version.Compare(addedInScheme) == LessThan {
-					return fmt.Errorf("field not supported: '%v'. added in: %v, config version: %v", typeField.Name, addedInScheme.String(), version.String())
-				}
-			}
-
-			// Check the "deprecatedIn" tag
-			tag = typeField.Tag.Get(tagDeprecatedIn)
-			if tag != "" {
-				deprecatedInScheme, err := NewSchemeVersion(tag)
-				if err != nil {
-					return err
-				}
-				// FIXME - perhaps an IsGreaterOrEqaul() function would be better
-				cmp := version.Compare(deprecatedInScheme)
-				if cmp == GreaterThan || cmp == EqualTo {
-					// FIXME - this should be a warning. need to figure out how best to return
-					// all errors/all warnings for validation methods
-					return fmt.Errorf("field deprecated: '%v'. deprecated in: %v, config version: %v", typeField.Name, deprecatedInScheme.String(), version.String())
-				}
-			}
-
-			// Check the "removedIn" tag
-			tag = typeField.Tag.Get(tagRemovedIn)
-			if tag != "" {
-				removedInScheme, err := NewSchemeVersion(tag)
-				if err != nil {
-					return err
-				}
-				// FIXME - perhaps an IsGreaterOrEqaul() function would be better
-				cmp := version.Compare(removedInScheme)
-				if cmp == GreaterThan || cmp == EqualTo {
-					// FIXME - this should be a warning. need to figure out how best to return
-					// all errors/all warnings for validation methods
-					return fmt.Errorf("field not supported: '%v'. removed in: %v, config version: %v", typeField.Name, removedInScheme.String(), version.String())
-				}
-			}
+func (validator *SchemeValidator) walk(v reflect.Value) error {
+	switch v.Kind() {
+	case reflect.Struct:
+		err := validator.walkStructFields(v)
+		if err != nil {
+			return err
 		}
 
-		// ------- END break --------
-
-		// For any nested types, go through and validate those as well.
-		switch field.Kind() {
-		case reflect.Struct:
-			// TODO
-		case reflect.Slice:
-			// TODO
-		case reflect.Ptr:
-			// TODO
+	case reflect.Slice:
+		for i := 0; i < v.Len(); i++ {
+			err := validator.walk(v.Index(i))
+			if err != nil {
+				return err
+			}
 		}
-
 	}
 	return nil
 }
 
-func isEmptyValue(v reflect.Value) bool {
+func (validator *SchemeValidator) walkStructFields(v reflect.Value) error {
+	t := v.Type()
+	for i := 0; i < t.NumField(); i++ {
+		field := v.Field(i)
+		structField := t.Field(i)
+
+		err := validator.validateField(field, structField)
+		if err != nil {
+			return err
+		}
+
+		// Try to walk through this field. If it is any nested type,
+		// it will go through and validate, otherwise it will return
+		// with no error.
+		err = validator.walk(field)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (validator *SchemeValidator) validateField(field reflect.Value, structField reflect.StructField) error {
+
+	version := validator.Version
+
+	// We should only care about validation if the field is set.
+	if !validator.isEmptyValue(field) {
+		// Check the "addedIn" tag
+		tag := structField.Tag.Get(tagAddedIn)
+		if tag != "" {
+			addedInScheme, err := NewSchemeVersion(tag)
+			if err != nil {
+				return err
+			}
+			// FIXME - perhaps an IsLessThan() function would be better
+			if version.Compare(addedInScheme) == LessThan {
+				return fmt.Errorf("field not supported: '%v'. added in: %v, config version: %v", structField.Name, addedInScheme.String(), version.String())
+			}
+		}
+
+		// Check the "deprecatedIn" tag
+		tag = structField.Tag.Get(tagDeprecatedIn)
+		if tag != "" {
+			deprecatedInScheme, err := NewSchemeVersion(tag)
+			if err != nil {
+				return err
+			}
+			// FIXME - perhaps an IsGreaterOrEqaul() function would be better
+			cmp := version.Compare(deprecatedInScheme)
+			if cmp == GreaterThan || cmp == EqualTo {
+				// FIXME - this should be a warning. need to figure out how best to return
+				// all errors/all warnings for validation methods
+				return fmt.Errorf("field deprecated: '%v'. deprecated in: %v, config version: %v", structField.Name, deprecatedInScheme.String(), version.String())
+			}
+		}
+
+		// Check the "removedIn" tag
+		tag = structField.Tag.Get(tagRemovedIn)
+		if tag != "" {
+			removedInScheme, err := NewSchemeVersion(tag)
+			if err != nil {
+				return err
+			}
+			// FIXME - perhaps an IsGreaterOrEqaul() function would be better
+			cmp := version.Compare(removedInScheme)
+			if cmp == GreaterThan || cmp == EqualTo {
+				// FIXME - this should be a warning. need to figure out how best to return
+				// all errors/all warnings for validation methods
+				return fmt.Errorf("field not supported: '%v'. removed in: %v, config version: %v", structField.Name, removedInScheme.String(), version.String())
+			}
+		}
+	}
+	return nil
+}
+
+// isEmptyValue checks if a value is its empty type.
+func (validator *SchemeValidator) isEmptyValue(v reflect.Value) bool {
 	switch v.Kind() {
 	case reflect.Array, reflect.Map, reflect.Slice, reflect.String:
 		return v.Len() == 0
