aeb2abade31fc52dbbd5dd1d8ce46e00e8229df5
Erick Daniszewski <edaniszewski@gmail.com>
2018-05-30 14:29:24 -0400

    update import ref

--- sdk/globals.go
+++ sdk/globals.go
@@ -1,15 +1,15 @@
 package sdk
 
 import (
-	"github.com/vapor-ware/synse-sdk/sdk/cfg"
+	"github.com/vapor-ware/synse-sdk/sdk/config"
 )
 
 // FIXME: there has to be a better way to set the config globally..
 // for now, this is fine since it gets things moving, but it needs
 // to be improved/fixed before v1.0 is considered done.
 
-var DeviceConfig cfg.DeviceConfig
-var PluginConfig cfg.PluginConfig
+var DeviceConfig config.DeviceConfig
+var PluginConfig config.PluginConfig
 
 // IsConfigured is a stand-in for a means by which we will determine whether
 // the plugin has been globally configured or not.
