df96798e3a09e16e8517ea2bc9c0f7fdc88616a0
Erick Daniszewski <edaniszewski@gmail.com>
2019-02-18 18:26:29 -0500

    checkpoint

--- sdk/scheduler.go
+++ sdk/scheduler.go
@@ -17,12 +17,20 @@
 package sdk
 
 import (
+	"context"
 	"sync"
 	"time"
+
+	"github.com/vapor-ware/synse-sdk/sdk/errors"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/vapor-ware/synse-sdk/sdk/config"
 	"golang.org/x/time/rate"
+)
+
+const (
+	modeSerial   = "serial"
+	modeParallel = "parallel"
 )
 
 // Scheduler is the plugin component which runs the read, write, and
@@ -30,6 +38,7 @@ import (
 type Scheduler struct {
 	// Plugin component references.
 	deviceManager *deviceManager
+	stateManager  *StateManager
 
 	// config is the configuration that is used by the scheduler.
 	config *config.PluginSettings
@@ -40,10 +49,14 @@ type Scheduler struct {
 
 	// limiter is a rate limiter for making requests.
 	limiter *rate.Limiter
+
+	// stop is a channel used to signal that the scheduler should stop.
+	// This is generally used for graceful shutdown.
+	stop chan struct{}
 }
 
 // NewScheduler creates a new instance of the plugin's scheduler component.
-func NewScheduler(conf *config.PluginSettings, deviceManager *deviceManager) *Scheduler {
+func NewScheduler(conf *config.PluginSettings, deviceManager *deviceManager, stateManager *StateManager) *Scheduler {
 	var limiter *rate.Limiter
 	// fixme: test if this is actually checking the same thing
 	if conf.Limiter != nil && conf.Limiter != (&config.LimiterSettings{}) {
@@ -56,10 +69,24 @@ func NewScheduler(conf *config.PluginSettings, deviceManager *deviceManager) *Sc
 
 	return &Scheduler{
 		deviceManager: deviceManager,
+		stateManager:  stateManager,
 		config:        conf,
 		limiter:       limiter,
 		serialLock:    &sync.Mutex{},
+		stop:          make(chan struct{}),
 	}
+}
+
+// registerActions registers pre-run (setup) and post-run (teardown) actions
+// for the scheduler.
+func (scheduler *Scheduler) registerActions(plugin *Plugin) {
+	// Register post-run actions.
+	plugin.RegisterPostRunActions(
+		&PluginAction{
+			Name:   "Stop scheduler",
+			Action: func(plugin *Plugin) error { return scheduler.Stop() },
+		},
+	)
 }
 
 // Start starts the scheduler.
@@ -67,6 +94,13 @@ func (scheduler *Scheduler) Start() {
 	go scheduler.scheduleReads()
 	go scheduler.scheduleWrites()
 	go scheduler.scheduleListen()
+}
+
+// Stop the scheduler.
+func (scheduler *Scheduler) Stop() error {
+	close(scheduler.stop)
+
+	return nil
 }
 
 // scheduleReads schedules device reads based on the plugin configuration.
@@ -98,6 +132,12 @@ func (scheduler *Scheduler) scheduleReads() {
 	// todo: figure out better way to handle this.. exiter channel??
 	rlog.Info("[scheduler] starting read scheduling")
 	for {
+		// If the stop channel is closed, stop the read loop.
+		select {
+		case <-scheduler.stop:
+			break
+		}
+
 		var waitGroup sync.WaitGroup
 
 		// Run all single device reads.
@@ -107,13 +147,22 @@ func (scheduler *Scheduler) scheduleReads() {
 
 			// Launch the device read.
 			go func(wg *sync.WaitGroup, device *Device) {
-				scheduler.read()
+				scheduler.read(device)
 				wg.Done()
 			}(&waitGroup, device)
 		}
 
 		// Run all batch device reads.
-		// TODO
+		for _, handler := range scheduler.deviceManager.handlers {
+			// Increment the WaitGroup for each bulk read action.
+			waitGroup.Add(1)
+
+			// Launch the bulk read.
+			go func(wg *sync.WaitGroup, handler *DeviceHandler) {
+				// todo: bulk read
+				wg.Done()
+			}(&waitGroup, handler)
+		}
 
 		if interval != 0 {
 			rlog.Debug("[scheduler] sleeping for read interval")
@@ -158,13 +207,60 @@ func (scheduler *Scheduler) scheduleListen() {
 	}
 }
 
-func (scheduler *Scheduler) read() {
+func (scheduler *Scheduler) read(device *Device) {
 	delay := scheduler.config.Read.Delay
 	mode := scheduler.config.Mode
 
 	rlog := log.WithFields(log.Fields{
-		"delay": delay,
-		"mode":  mode,
+		"delay":  delay,
+		"mode":   mode,
+		"device": device.ID(),
 	})
+
+	// Rate limiting, if configured. We want to do this before potentially
+	// acquiring the serial lock so something isn't holding on to the lock
+	// and just waiting.
+	if scheduler.limiter != nil {
+		if err := scheduler.limiter.Wait(context.Background()); err != nil {
+			rlog.WithField("error", err).Error("[scheduler] error with rate limiter")
+		}
+	}
+
+	// If we are running in serial mode, acquire the serial lock.
+	if mode == modeSerial {
+		scheduler.serialLock.Lock()
+		defer scheduler.serialLock.Unlock()
+	}
+	// fixme: think about where this goes w.r.t the bulk read check...
+
+	// If the device does not get its readings from a bulk read operation, then
+	// it is read individually. If a device is read in bulk, it will not be read
+	// here; it will be read later via the bulkRead function.
+	if !device.handler.supportsBulkRead() {
+		response, err := device.Read()
+		if err != nil {
+			// Check to see if the error is that of unsupported error. If it is, we
+			// do not want to log out here (low-interval read polling would cause this
+			// to pollute the logs for something that we should already know).
+			_, unsupported := err.(*errors.UnsupportedCommandError)
+			if !unsupported {
+				rlog.Error("[scheduler] failed device read")
+			}
+		} else {
+			scheduler.stateManager.readChan <- response
+		}
+
+		// If a delay is configured, wait for the delay before continuing
+		// (and relinquishing the lock, if in serial mode).
+		if delay != 0 {
+			rlog.Debug("[scheduler] sleeping for read delay")
+			time.Sleep(delay)
+			rlog.Debug("[scheduler] waking up for read delay")
+		}
+	}
+
+}
+
+func (scheduler *Scheduler) bulkRead(handler *DeviceHandler) {
 
 }
