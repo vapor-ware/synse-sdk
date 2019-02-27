555e01895c372ff00de5933575eddf92e846d643
Erick Daniszewski <edaniszewski@gmail.com>
2019-02-27 12:57:15 -0500

    add unit tests, pt. 3 (#384)

--- sdk/options_test.go
+++ sdk/options_test.go
@@ -16,69 +16,112 @@
 
 package sdk
 
-//
-//import (
-//	"testing"
-//
-//	"github.com/stretchr/testify/assert"
-//)
-//
-//// TestCustomDeviceIdentifier tests creating a PluginOption for a custom
-//// device identifier.
-//func TestCustomDeviceIdentifier(t *testing.T) {
-//	opt := CustomDeviceIdentifier(
-//		func(data map[string]interface{}) string {
-//			return "foo"
-//		},
-//	)
-//	ctx := PluginContext{}
-//	assert.Nil(t, ctx.deviceIdentifier)
-//
-//	opt(&ctx)
-//	assert.NotNil(t, ctx.deviceIdentifier)
-//}
-//
-//// TestCustomDynamicDeviceRegistration tests creating a PluginOption for
-//// a custom device registration function.
-//func TestCustomDynamicDeviceRegistration(t *testing.T) {
-//	opt := CustomDynamicDeviceRegistration(
-//		func(data map[string]interface{}) ([]*Device, error) {
-//			return []*Device{}, nil
-//		},
-//	)
-//	ctx := PluginContext{}
-//	assert.Nil(t, ctx.dynamicDeviceRegistrar)
-//
-//	opt(&ctx)
-//	assert.NotNil(t, ctx.dynamicDeviceRegistrar)
-//}
-//
-//// TestCustomDynamicDeviceConfigRegistration tests creating a PluginOption
-//// for a custom device config registration function.
-//func TestCustomDynamicDeviceConfigRegistration(t *testing.T) {
-//	opt := CustomDynamicDeviceConfigRegistration(
-//		func(data map[string]interface{}) ([]*DeviceConfig, error) {
-//			return []*DeviceConfig{}, nil
-//		},
-//	)
-//	ctx := PluginContext{}
-//	assert.Nil(t, ctx.dynamicDeviceConfigRegistrar)
-//
-//	opt(&ctx)
-//	assert.NotNil(t, ctx.dynamicDeviceConfigRegistrar)
-//}
-//
-//// TestCustomDeviceDataValidator tests creating a PluginOption for a custom
-//// device data validator function.
-//func TestCustomDeviceDataValidator(t *testing.T) {
-//	opt := CustomDeviceDataValidator(
-//		func(i map[string]interface{}) error {
-//			return nil
-//		},
-//	)
-//	ctx := PluginContext{}
-//	assert.Nil(t, ctx.deviceDataValidator)
-//
-//	opt(&ctx)
-//	assert.NotNil(t, ctx.deviceDataValidator)
-//}
+import (
+	"testing"
+
+	"github.com/vapor-ware/synse-sdk/sdk/config"
+	"github.com/vapor-ware/synse-sdk/sdk/policy"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TestCustomDeviceIdentifier tests creating a PluginOption for a custom
+// device identifier.
+func TestCustomDeviceIdentifier(t *testing.T) {
+	opt := CustomDeviceIdentifier(
+		func(data map[string]interface{}) string {
+			return "foo"
+		},
+	)
+	plugin := Plugin{
+		pluginHandlers: &PluginHandlers{},
+	}
+	assert.Nil(t, plugin.pluginHandlers.DeviceIdentifier)
+
+	opt(&plugin)
+	assert.NotNil(t, plugin.pluginHandlers.DeviceIdentifier)
+}
+
+// TestCustomDynamicDeviceRegistration tests creating a PluginOption for
+// a custom device registration function.
+func TestCustomDynamicDeviceRegistration(t *testing.T) {
+	opt := CustomDynamicDeviceRegistration(
+		func(data map[string]interface{}) ([]*Device, error) {
+			return []*Device{}, nil
+		},
+	)
+	plugin := Plugin{
+		pluginHandlers: &PluginHandlers{},
+	}
+	assert.Nil(t, plugin.pluginHandlers.DynamicRegistrar)
+
+	opt(&plugin)
+	assert.NotNil(t, plugin.pluginHandlers.DynamicRegistrar)
+}
+
+// TestCustomDynamicDeviceConfigRegistration tests creating a PluginOption
+// for a custom device config registration function.
+func TestCustomDynamicDeviceConfigRegistration(t *testing.T) {
+	opt := CustomDynamicDeviceConfigRegistration(
+		func(data map[string]interface{}) ([]*config.DeviceProto, error) {
+			return []*config.DeviceProto{}, nil
+		},
+	)
+	plugin := Plugin{
+		pluginHandlers: &PluginHandlers{},
+	}
+	assert.Nil(t, plugin.pluginHandlers.DynamicConfigRegistrar)
+
+	opt(&plugin)
+	assert.NotNil(t, plugin.pluginHandlers.DynamicConfigRegistrar)
+}
+
+// TestCustomDeviceDataValidator tests creating a PluginOption for a custom
+// device data validator function.
+func TestCustomDeviceDataValidator(t *testing.T) {
+	opt := CustomDeviceDataValidator(
+		func(i map[string]interface{}) error {
+			return nil
+		},
+	)
+	plugin := Plugin{
+		pluginHandlers: &PluginHandlers{},
+	}
+	assert.Nil(t, plugin.pluginHandlers.DeviceDataValidator)
+
+	opt(&plugin)
+	assert.NotNil(t, plugin.pluginHandlers.DeviceDataValidator)
+}
+
+func TestPluginConfigRequired(t *testing.T) {
+	opt := PluginConfigRequired()
+	plugin := Plugin{
+		policies: &policy.Policies{},
+	}
+	assert.Empty(t, plugin.policies.PluginConfig)
+
+	opt(&plugin)
+	assert.Equal(t, policy.Required, plugin.policies.PluginConfig)
+}
+
+func TestDeviceConfigOptional(t *testing.T) {
+	opt := DeviceConfigOptional()
+	plugin := Plugin{
+		policies: &policy.Policies{},
+	}
+	assert.Empty(t, plugin.policies.DeviceConfig)
+
+	opt(&plugin)
+	assert.Equal(t, policy.Optional, plugin.policies.DeviceConfig)
+}
+
+func TestDynamicConfigRequired(t *testing.T) {
+	opt := DynamicConfigRequired()
+	plugin := Plugin{
+		policies: &policy.Policies{},
+	}
+	assert.Empty(t, plugin.policies.DynamicDeviceConfig)
+
+	opt(&plugin)
+	assert.Equal(t, policy.Required, plugin.policies.DynamicDeviceConfig)
+}
