6280d67a6c01cbfa9e3f243ecbacf62c12222a75
Erick Daniszewski <edaniszewski@gmail.com>
2019-03-12 09:23:57 -0400

    state manager tests

--- sdk/state_manager_test.go
+++ sdk/state_manager_test.go
@@ -17,11 +17,15 @@
 package sdk
 
 import (
+	"sync"
 	"testing"
 	"time"
 
+	"github.com/patrickmn/go-cache"
 	"github.com/stretchr/testify/assert"
 	"github.com/vapor-ware/synse-sdk/sdk/config"
+	"github.com/vapor-ware/synse-sdk/sdk/health"
+	"github.com/vapor-ware/synse-sdk/sdk/output"
 )
 
 func Test_newStateManager_nilConfig(t *testing.T) {
@@ -66,4 +70,113 @@ func TestStateManager_registerActions(t *testing.T) {
 	sm.registerActions(&plugin)
 
 	assert.Len(t, plugin.preRun, 1)
+}
+
+func TestStateManager_healthChecks(t *testing.T) {
+	plugin := Plugin{
+		health: health.NewManager(&config.HealthSettings{}),
+	}
+	sm := stateManager{}
+
+	assert.Equal(t, plugin.health.Count(), 0)
+
+	err := sm.healthChecks(&plugin)
+	assert.NoError(t, err)
+
+	assert.Equal(t, plugin.health.Count(), 1)
+}
+
+func TestStateManager_addReadingToCache_cacheDisabled(t *testing.T) {
+	sm := stateManager{
+		config: &config.PluginSettings{
+			Cache: &config.CacheSettings{
+				Enabled: false,
+			},
+		},
+		readingsCache: cache.New(1*time.Minute, 2*time.Minute),
+	}
+
+	assert.Equal(t, 0, sm.readingsCache.ItemCount())
+
+	sm.addReadingToCache(&ReadContext{
+		Device:  "test-1",
+		Reading: []*output.Reading{{Value: 1}},
+	})
+
+	assert.Equal(t, 0, sm.readingsCache.ItemCount())
+
+	_, exists := sm.readingsCache.Get("test-1")
+	assert.False(t, exists)
+}
+
+func TestStateManager_addReadingToCache_new(t *testing.T) {
+	sm := stateManager{
+		config: &config.PluginSettings{
+			Cache: &config.CacheSettings{
+				Enabled: true,
+			},
+		},
+		readingsCache: cache.New(1*time.Minute, 2*time.Minute),
+	}
+
+	assert.Equal(t, 0, sm.readingsCache.ItemCount())
+
+	sm.addReadingToCache(&ReadContext{
+		Device:  "test-1",
+		Reading: []*output.Reading{{Value: 1}},
+	})
+
+	assert.Equal(t, 1, sm.readingsCache.ItemCount())
+}
+
+func TestStateManager_addReadingToCache_twoReadings(t *testing.T) {
+	sm := stateManager{
+		config: &config.PluginSettings{
+			Cache: &config.CacheSettings{
+				Enabled: true,
+			},
+		},
+		readingsCache: cache.New(1*time.Minute, 2*time.Minute),
+	}
+
+	assert.Equal(t, 0, sm.readingsCache.ItemCount())
+
+	// Add first reading
+	sm.addReadingToCache(&ReadContext{
+		Device:  "test-1",
+		Reading: []*output.Reading{{Value: 1}},
+	})
+
+	assert.Equal(t, 1, sm.readingsCache.ItemCount())
+
+	// Add second reading for the device
+	sm.addReadingToCache(&ReadContext{
+		Device:  "test-1",
+		Reading: []*output.Reading{{Value: 1}},
+	})
+
+	assert.Equal(t, 1, sm.readingsCache.ItemCount())
+}
+
+func TestStateManager_GetReadingsForDevice_noDevice(t *testing.T) {
+	sm := stateManager{
+		readingsLock: &sync.RWMutex{},
+		readings:     map[string][]*output.Reading{},
+	}
+
+	res := sm.GetReadingsForDevice("foo")
+	assert.Nil(t, res)
+}
+
+func TestStateManager_GetReadingsForDevice_deviceExists(t *testing.T) {
+	sm := stateManager{
+		readingsLock: &sync.RWMutex{},
+		readings: map[string][]*output.Reading{
+			"foo": {{Value: 1}},
+		},
+	}
+
+	res := sm.GetReadingsForDevice("foo")
+	assert.NotNil(t, res)
+	assert.Len(t, res, 1)
 }
