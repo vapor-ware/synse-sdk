00038d2fcc68ee703fcc4c01471ac53887adf9d6
Erick Daniszewski <edaniszewski@gmail.com>
2019-04-04 13:49:47 -0400

    updates for recent grpc changes/fixes (#405)

--- examples/multi_device_plugin/devices/air8884.go
+++ examples/multi_device_plugin/devices/air8884.go
@@ -14,4 +14,10 @@ var Air8884 = sdk.DeviceHandler{
 		reading := outputs.AirflowOutput.MakeReading(100)
 		return []*output.Reading{reading}, nil
 	},
+	Write: func(device *sdk.Device, data *sdk.WriteData) error {
+		// Defines a write function which does nothing -- this is just so
+		// the example plugin has a writable device, which may be useful
+		// for debugging/testing.
+		return nil
+	},
 }
